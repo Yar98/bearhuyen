2f6b24a208cf26c5d9ca8a4ab444ecf7c22b9052
Yar98 <tienfu98@gmail.com>
2025-11-05 07:38:39 +0700

    demo add heart rain

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 
-var isOpen = false;
+function createHeart() {
+  const heart = document.createElement('div');
+  heart.classList.add('heart');
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+  heart.innerText = '💗';
+
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   var pages = document.getElementsByClassName('page');
@@ -9,22 +23,21 @@ document.addEventListener('DOMContentLoaded', function () {
       page.style.zIndex = (pages.length - i);
     }
   }
-  for(var i = 0; i < pages.length; i++)
-      {
-        //Or var page = pages[i];
-        pages[i].pageNum = i + 1;
-        pages[i].onclick=function()
-          {
-            if (this.pageNum % 2 === 0)
-              {
-                this.classList.remove('flipped');
-                this.previousElementSibling.classList.remove('flipped');
-              }
-            else
-              {
-                this.classList.add('flipped');
-                this.nextElementSibling.classList.add('flipped');
-              }
-           }
-        }
+  for (var i = 0; i < pages.length; i++) {
+    //Or var page = pages[i];
+    pages[i].pageNum = i + 1;
+    pages[i].onclick = function () {
+      if (this.pageNum % 2 === 0) {
+        this.classList.remove('flipped');
+        this.previousElementSibling.classList.remove('flipped');
+      }
+      else {
+        this.classList.add('flipped');
+        this.nextElementSibling.classList.add('flipped');
+      }
+    }
+  }
+
+  setInterval(createHeart, 300);
 })
+
