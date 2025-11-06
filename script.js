83da5db6e3c21c3ddb797252a9188be090831f28
Yar98 <tienfu98@gmail.com>
2025-11-06 07:59:26 +0700

    change heart color

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function createHeart() {
   heart.style.left = Math.random() * 100 + "vw";
   heart.style.animationDuration = Math.random() * 2 + 3 + "s";
 
-  heart.innerText = '💗';
+  heart.innerText = '❤️';
 
   document.body.appendChild(heart);
 
